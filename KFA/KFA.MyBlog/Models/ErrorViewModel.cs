4908367e5d22ca91b926b0e0ae918eb42d50d6a2
Fedor <fedor_ka@mail.ru>
2026-02-20 16:52:36 +0300

    start

---
+++ KFA/KFA.MyBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KFA.MyBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
